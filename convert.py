0fea8af81cfb9df500c49d96b913865a59dfd54f
Baruch Rothkoff <baruchiro@gmail.com>
2018-07-02 07:53:32 +0300

    add convert method

---
+++ convert.py
@@ -0,0 +1,13 @@
+import numpy as np
+
+
+def get_bitmap_from_bmp(path: str) -> np.ndarray:
+    """Method to read .bmp file and return the image bitmap
+
+    Arguments:
+        path {str} -- The .bmp file path
+
+    Returns:
+        np.ndarray -- 2D array that present the bitmap
+    """
+    pass
