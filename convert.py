a75b510c6b419c2c704492e8dc10e687d41b985c
Baruch Rothkoff <baruchiro@gmail.com>
2018-07-06 14:24:44 +0300

    Add method and tests

--- convert.py
+++ convert.py
@@ -1,21 +1,35 @@
 import sys
-import numpy as np
+from numpy import ndarray
 from PIL import Image
 
 
-def get_bitmap_from_bmp(path: str) -> np.ndarray:
+def get_bitmap_from_bmp(path: str) -> ndarray:
     """Method to read .bmp file and return the image bitmap
 
     Arguments:
         path {str} -- The .bmp file path
 
     Returns:
-        np.ndarray -- 2D array that present the bitmap
+        np.ndarray -- 2D array that present the bitmap,
+        each pixel is array of [R,G,B]
     """
 
     # HowTo:
     # Search "python read image matrix with PIL"
     # Search "python PIL image to numpy array"
+
+    pass
+
+
+def RGB_to_YCbCr(matrix: ndarray)->ndarray:
+    """Converting pixels from RGB (Red, Green, Blue) to YCbCr (luma, blue-difference, red-difference)
+
+    Arguments:
+        matrix {ndarray} -- The image Bitmap as 2D array
+
+    Returns:
+        ndarray -- The new Bitmap with YCbCr as 2D array
+    """
 
     pass
 
--- tests.py
+++ tests.py
@@ -1,7 +1,34 @@
 import unittest
+import convert
+import numpy as np
+from matplotlib import pyplot
+import os
+
+path = os.getcwd()
 
 
-class ConvertingTest(unittest.TestCase):
+class Test_get_bitmap_from_bmp(unittest.TestCase):
+
+    @staticmethod
+    def __generate_image_matrix(x, y, pixel):
+        return np.array([[pixel for i in range(y)]for j in range(x)])
+
     @unittest.expectedFailure
-    def test_convertBitmap_getMatrix(self):
-        self.assertEqual(1, 2)
+    def test_whiteImage_getAllWhite(self):
+        test_matrix = self.__generate_image_matrix(
+            8**3, 8**3, [255, 255, 255])
+        np.testing.assert_array_equal(
+            convert.get_bitmap_from_bmp(os.path.join("img", "white.bmp")),
+            test_matrix)
+
+    @unittest.expectedFailure
+    def test_BlackImage_getAllWhite(self):
+        test_matrix = self.__generate_image_matrix(
+            8**3, 8**3, [0, 0, 0])
+        np.testing.assert_array_equal(
+            convert.get_bitmap_from_bmp(os.path.join("img", "black.bmp")),
+            test_matrix)
+
+
+if __name__ == "__main__":
+    unittest.main()
