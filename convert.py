28f6e860168bd15f0156bfa42c1d925b92c708b8
Baruch Rothkoff <baruchiro@gmail.com>
2018-07-08 07:30:13 +0300

    add tcbcr downstream

--- convert.py
+++ convert.py
@@ -33,6 +33,17 @@ def RGB_to_YCbCr(matrix: ndarray)->ndarray:
 
     pass
 
+def YCbCr_Downstream(matrix:ndarray)->ndarray:
+    """Downstream the Cb and Cr with 4:2:0 correlation
+    
+    Arguments:
+        matrix {ndarray} -- The image matrix as YCbCr
+    
+    Returns:
+        ndarray -- The new image matrix with downstreamed YCbCr
+    """
+
+    pass
 
 if __name__ == "__main__":
     get_bitmap_from_bmp(sys.argv[0])
