535b2767417de3f581d7e0a8f768add7ed66c1b8
Baruch Rothkoff <baruchiro@gmail.com>
2018-07-08 21:23:58 +0300

    add function generators to tests

--- tests.py
+++ tests.py
@@ -7,15 +7,19 @@ import os
 path = os.getcwd()
 
 
-class Test_get_bitmap_from_bmp(unittest.TestCase):
+def get_colored_matrix(x, y):
+    return np.array([[[row % 255, col % 255, (row+col) % 255] for col in range(y)] for row in range(x)])
 
-    @staticmethod
-    def __generate_image_matrix(x, y, pixel):
-        return np.array([[pixel for i in range(y)]for j in range(x)])
+
+def generate_one_color_matrix(x, y, pixel):
+    return np.array([[pixel for i in range(y)]for j in range(x)])
+
+
+class Test_get_bitmap_from_bmp(unittest.TestCase):
 
     @unittest.expectedFailure
     def test_whiteImage_getAllWhite(self):
-        test_matrix = self.__generate_image_matrix(
+        test_matrix = generate_one_color_matrix(
             8**3, 8**3, [255, 255, 255])
         np.testing.assert_array_equal(
             convert.get_bitmap_from_bmp(os.path.join("img", "white.bmp")),
@@ -23,10 +27,17 @@ class Test_get_bitmap_from_bmp(unittest.TestCase):
 
     @unittest.expectedFailure
     def test_BlackImage_getAllWhite(self):
-        test_matrix = self.__generate_image_matrix(
+        test_matrix = generate_one_color_matrix(
             8**3, 8**3, [0, 0, 0])
         np.testing.assert_array_equal(
             convert.get_bitmap_from_bmp(os.path.join("img", "black.bmp")),
+            test_matrix)
+
+    @unittest.expectedFailure
+    def test_ColoredImage_getAllWhite(self):
+        test_matrix = get_colored_matrix(255, 255)
+        np.testing.assert_array_equal(
+            convert.get_bitmap_from_bmp(os.path.join("img", "colored.bmp")),
             test_matrix)
 
 
