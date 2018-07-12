23f7a7758c45b46659dfae95bd3248eeea536527
Baruch Rothkoff <baruchiro@gmail.com>
2018-07-12 18:11:16 +0300

    yapf

--- convert.py
+++ convert.py
@@ -4,15 +4,15 @@ from PIL import Image
 from matplotlib import image, pyplot
 
 
-def get_bitmap_from_bmp(
-    path: str)->np.ndarray: return image.imread(path)
+def get_bitmap_from_bmp(path: str) -> np.ndarray:
+    return image.imread(path)
 
 
 def rgb_pixel_to_ycbcr(r: int, g: int, b: int):
     return [
-        int(round(0 + .299 * r + .587 * g + .114 * b)),         # Y'
-        int(round(128 - .168736 * r - .331264 * g + .5 * b)),    # Cb
-        int(round(128 + .5 * r - .418688 * g - .081312 * b))    # Cr
+        int(round(0 + .299 * r + .587 * g + .114 * b)),  # Y'
+        int(round(128 - .168736 * r - .331264 * g + .5 * b)),  # Cb
+        int(round(128 + .5 * r - .418688 * g - .081312 * b))  # Cr
     ]
 
 
@@ -25,7 +25,8 @@ def RGB_to_YCbCr(matrix):
     Returns:
         ndarray -- The new Bitmap with YCbCr as 2D array
     """
-    return ((rgb_pixel_to_ycbcr(col[0], col[1], col[2]) for col in row) for row in matrix)
+    return ((rgb_pixel_to_ycbcr(col[0], col[1], col[2]) for col in row)
+            for row in matrix)
 
 
 def YCbCr_Downstream(matrix):
@@ -38,12 +39,11 @@ def YCbCr_Downstream(matrix):
         ndarray -- The new image matrix with downstreamed YCbCr
     """
 
-    pass
 
 # split into Y, Cb, Cr?? where? what is more helpfull?
 
 
-def split_matrix_into_submatrixs(matrix: np.ndarray)->tuple:
+def split_matrix_into_submatrixs(matrix):
     """Split the bitmap to 8*8 matrixs
 
     Arguments:
@@ -52,11 +52,11 @@ def split_matrix_into_submatrixs(matrix: np.ndarray)->tuple:
     Returns:
         list -- list of all 8*8 ndarrays matrix
     """
-
+    
     pass
 
 
-def centering_values_to_zero(submatrix: np.ndarray)->np.ndarray:
+def centering_values_to_zero(submatrix: np.ndarray) -> np.ndarray:
     """Normalize YCbCr values- remove 128 from each object
 
     Arguments:
@@ -69,7 +69,7 @@ def centering_values_to_zero(submatrix: np.ndarray)->np.ndarray:
     pass
 
 
-def discerete_cosine_transform(submatrix: np.ndarray)->np.ndarray:
+def discerete_cosine_transform(submatrix: np.ndarray) -> np.ndarray:
     """Calculate the DCT that discaide here- https://en.wikipedia.org/wiki/JPEG#Discrete_cosine_transform
 
     Arguments:
@@ -82,7 +82,7 @@ def discerete_cosine_transform(submatrix: np.ndarray)->np.ndarray:
     pass
 
 
-def quantization(submatrix: np.ndarray)->np.ndarray:
+def quantization(submatrix: np.ndarray) -> np.ndarray:
     pass
 
 
