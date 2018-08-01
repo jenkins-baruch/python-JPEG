8725bd76f421691e0eec435a3237f8c5eefd1d70
Baruch Rothkoff <baruchiro@gmail.com>
2018-08-01 03:29:52 +0300

    try to save jpeg

--- Flowchart/generator.py
+++ Flowchart/generator.py
@@ -5,9 +5,15 @@ sys.path.insert(0, root_dir)
 os.chdir(root_dir)
 import imagetools
 import encode
+import cv2
 
 path = sys.argv[1]
 img_matrix = imagetools.get_bitmap_from_bmp(path)
-#imagetools.save_matrix(img_matrix, mode='RGB', dest='img/rgb.bmp')
+
 ycbcr = encode.RGB_to_YCbCr(img_matrix)
-imagetools.save_matrix(ycbcr, mode="YCbCr", dest="")
+imagetools.show_matrix(ycbcr, mode='YCbCr')
+imagetools.save_matrix(ycbcr, mode="YCbCr",
+                       dest="Flowchart/results/ycbcr.jpeg")
+jpg = imagetools.get_bitmap_from_bmp("Flowchart/results/ycbcr.jpeg")
+input()
+imagetools.show_matrix(jpg, mode='YCbCr')
--- imagetools.py
+++ imagetools.py
@@ -2,8 +2,14 @@ import numpy as np
 from PIL import Image
 from matplotlib import image, pyplot
 
+
 def get_bitmap_from_bmp(path: str) -> np.ndarray:
     return image.imread(path)
 
-def save_matrix(matrix: np.ndarray,*, mode:str, dest:str):
+
+def save_matrix(matrix: np.ndarray, *, mode: str, dest: str):
+    Image.fromarray(matrix, mode=mode).save(dest)
+
+
+def show_matrix(matrix: np.ndarray, *, mode: str):
     Image.fromarray(matrix, mode=mode).show()
