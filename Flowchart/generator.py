594a464d4815ffdc8b0feac965cb639a4cc2f2a6
Baruch Rothkoff <baruchiro@gmail.com>
2018-08-12 17:08:28 +0300

    comment

--- Flowchart/generator.py
+++ Flowchart/generator.py
@@ -1,16 +1,20 @@
 import sys
 import os
+import cv2
+
+# import from project folder
 root_dir = os.path.dirname(os.path.dirname(__file__))
 sys.path.insert(0, root_dir)
 os.chdir(root_dir)
 import imagetools
 import encode
-import cv2
+
+
 
 path = sys.argv[1]
 img_matrix = imagetools.get_bitmap_from_bmp(path)
 
-ycbcr = encode.RGB_to_YCbCr(img_matrix)
+ycbcr = imagetools.RGB_to_YCbCr(img_matrix)
 imagetools.show_matrix(ycbcr, mode='YCbCr')
 imagetools.save_matrix(ycbcr, mode="YCbCr",
                        dest="Flowchart/results/ycbcr.jpeg")
