6d0bf6d061bb7c8324349ac9da00832ed7f13299
Baruch Rothkoff <baruchiro@gmail.com>
2018-08-16 11:04:39 +0300

    Add more flow

--- Flowchart/generator.py
+++ Flowchart/generator.py
@@ -15,35 +15,46 @@ import encode
 path = "img/moto.png"# sys.argv[1]
 print("Read image " + path)
 img = imagetools.get_bitmap_from_bmp(path)
+imagetools.save_matrix(img, dest=os.path.join(dirname, 'original.png'))
 
 # Convert to YCrCb
 print("Convert to YCrCb")
 img_ycrcb = imagetools.BGR_to_YCrCb(img)
 
 print("Seperate colors")
-img_ycbcr_y, img_ycbcr_cr, img_ycbcr_cb = encode.seperate_to_three_colors(img_ycrcb)
-img_b, img_g, img_r = encode.seperate_to_three_colors(img)
+img_channel_y, img_channel_cr, img_channel_cb = encode.seperate_to_three_colors(img_ycrcb)
+img_channel_b, img_channel_g, img_channel_r = encode.seperate_to_three_colors(img)
 
 print("Const matrixes")
-ycrcb_channel_const = np.ones_like(img_ycbcr_y) * 127.5
-gbr_channel_const = np.zeros_like(img_b)
+ycrcb_channel_const = np.ones_like(img_channel_y) * 127.5
+gbr_channel_const = np.zeros_like(img_channel_b)
 
 print("Generate YCrCb colors images")
-img_ycbcr_y = encode.concatenate_three_colors(img_ycbcr_y, ycrcb_channel_const, ycrcb_channel_const)
-img_ycbcr_cr = encode.concatenate_three_colors(ycrcb_channel_const, img_ycbcr_cr, ycrcb_channel_const)
-img_ycbcr_cb = encode.concatenate_three_colors(ycrcb_channel_const, ycrcb_channel_const, img_ycbcr_cb)
+img_matrix_y = encode.concatenate_three_colors(img_channel_y, ycrcb_channel_const, ycrcb_channel_const)
+img_matrix_cr = encode.concatenate_three_colors(ycrcb_channel_const, img_channel_cr, ycrcb_channel_const)
+img_matrix_cb = encode.concatenate_three_colors(ycrcb_channel_const, ycrcb_channel_const, img_channel_cb)
 
-imagetools.save_matrix(img_ycbcr_y, mode='YCrCb', dest=os.path.join(dirname, 'channel_y.png'))
-imagetools.save_matrix(img_ycbcr_cr, mode='YCrCb', dest=os.path.join(dirname, 'channel_cr.png'))
-imagetools.save_matrix(img_ycbcr_cb, mode='YCrCb', dest=os.path.join(dirname, 'channel_cb.png'))
+imagetools.save_matrix(img_matrix_y, mode='YCrCb', dest=os.path.join(dirname, 'channel_y.png'))
+imagetools.save_matrix(img_matrix_cr, mode='YCrCb', dest=os.path.join(dirname, 'channel_cr.png'))
+imagetools.save_matrix(img_matrix_cb, mode='YCrCb', dest=os.path.join(dirname, 'channel_cb.png'))
 
 print("Generate BGR colors images")
-img_b = encode.concatenate_three_colors(img_b, gbr_channel_const, gbr_channel_const)
-img_g = encode.concatenate_three_colors(gbr_channel_const, img_g, gbr_channel_const)
-img_r = encode.concatenate_three_colors(gbr_channel_const, gbr_channel_const, img_r)
+img_matrix_b = encode.concatenate_three_colors(img_channel_b, gbr_channel_const, gbr_channel_const)
+img_matrix_g = encode.concatenate_three_colors(gbr_channel_const, img_channel_g, gbr_channel_const)
+img_matrix_r = encode.concatenate_three_colors(gbr_channel_const, gbr_channel_const, img_channel_r)
 
-imagetools.save_matrix(img_b, dest=os.path.join(dirname, 'channel_b.png'))
-imagetools.save_matrix(img_g, dest=os.path.join(dirname, 'channel_g.png'))
-imagetools.save_matrix(img_r,dest=os.path.join(dirname, 'channel_r.png'))
+imagetools.save_matrix(img_matrix_b, dest=os.path.join(dirname, 'channel_b.png'))
+imagetools.save_matrix(img_matrix_g, dest=os.path.join(dirname, 'channel_g.png'))
+imagetools.save_matrix(img_matrix_r, dest=os.path.join(dirname, 'channel_r.png'))
+
+print("Downsapling")
+img_channel_cb = encode.Upsample(encode.Downsample(img_channel_cb))
+img_channel_cr = encode.Upsample(encode.Downsample(img_channel_cr))
+
+img_channel_g = encode.Upsample(encode.Downsample(img_channel_g))
+img_channel_r = encode.Upsample(encode.Downsample(img_channel_r))
+
+imagetools.save_matrix(encode.concatenate_three_colors(img_channel_y, img_channel_cr, img_channel_cb), mode='YCrCb', dest=os.path.join(dirname, 'ycrcb_downsapling.png'))
+imagetools.save_matrix(encode.concatenate_three_colors(img_channel_b, img_channel_g, img_channel_r), dest=os.path.join(dirname, 'bgr_downsapling.png'))
 
 input("END")
--- README.md
+++ README.md
@@ -1,4 +1,44 @@
 # JPEG Algorithm with Python
-# By [Meny Buganim](@menybgu), Lital Hayat and [Baruch Rothkoff](@baruchiro)
+
+## By [Meny Buganim](todo), [Lital Hayat](todo) and [Baruch Rothkoff](todo)
+
+### Build status:
 
 [![Build Status](http://54.218.224.31:8080/job/python-JPEG/job/master/badge/icon)](http://54.218.224.31:8080/job/python-JPEG/job/master/)
+
+## Flow and explanation of JPEG algorithm
+
+Note that because during the work we compare our results vs [cv2 library (OpenCV for Python)](TODO), who is display color in different order, our colors represented as **BGR** (instead of **RGB**) and **Y'CrCb** (instead of **Y'CbCr**)
+
+### Original Image
+
+<img src="Flowchart/original.png" width="100%">
+
+### Convert to Y'CrCb
+
+Y'CrCb is channels represention as luma component, blue-difference and red-difference ([from wikipedia](https://en.wikipedia.org/wiki/YCbCr)).
+
+We know that __B__GR is  Blue, Green and Red channels, like this:
+<img src="Flowchart/channel_b.png" width="30%">
+<img src="Flowchart/channel_g.png" width="30%">
+<img src="Flowchart/channel_r.png" width="30%">
+
+But Y'CrCb shouls be different division (Without lossing the data!):
+
+<img src="Flowchart/channel_y.png" width="30%">
+<img src="Flowchart/channel_cr.png" width="30%">
+<img src="Flowchart/channel_cb.png" width="30%">
+
+We do this because the humen eyes are more luma sensitive to red and blue, so
+
+### Downsampling
+
+we can remove data from blue and red, and the impact of whole image will be less than downsapling BGR image, like that:
+
+#### BGR Downsapling
+
+<img src="Flowchart/bgr_downsapling.png" width="1000%">
+
+#### Y'CrCb
+
+<img src="Flowchart/ycrcb_downsapling.png" width="1000%">
